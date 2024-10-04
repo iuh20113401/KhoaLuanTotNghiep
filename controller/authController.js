7278a38549a7e01bfa9ccd73fdc8e67e1fdb54f2
iuh20113401 <kiet5454aa@gmail.com>
2024-10-04 09:46:26 +0700

    first commit

--- controller/authController.js
+++ controller/authController.js
@@ -106,10 +106,15 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 exports.logout = (req, res) => {
-  res.cookie('jwt', 'loggedout', {
+  const options = {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
-  });
+  };
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+    options.sameSite = 'None';
+  }
+  res.cookie('jwt', 'loggedout', options);
   res.status(200).json({ status: 'success', data: { status: 'success' } });
 };
 
